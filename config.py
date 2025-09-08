f77d3cf6f90be27fccd3e96317b3441a6fce8f31
wilawan047 <computersci65@gmail.com>
2025-09-08 18:05:02 +0700

    Update config for Railway DB and add scrypt to requirements

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ class Config:
     # NOTE: ใช้ port 3307 ห้ามแก้กลับเป็น 3306
  
     SECRET_KEY = os.environ.get('SECRET_KEY', 'defaultsecret')
-    DB_HOST = os.environ.get('DB_HOST', 'localhost')
-    DB_PORT = int(os.environ.get('DB_PORT', 3307))
+    DB_HOST = os.environ.get('DB_HOST', 'switchyard.proxy.rlwy.net')
+    DB_PORT = int(os.environ.get('DB_PORT', 21922))
     DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
-    DB_NAME = os.environ.get('DB_NAME', 'tire_shop')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'mxAiijYOvjVtdUrdtVCVyMygyvxOFOhO')
+    DB_NAME = os.environ.get('DB_NAME', 'railway')
 
     # File Upload Configuration
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'uploads')
